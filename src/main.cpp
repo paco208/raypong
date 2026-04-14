c4cc792ced9c86c0825b4620f6892302e918caf9
Jakob <jakob.ferjancic@gmail.com>
2026-04-14 10:38:36 +0200

    Dodano premikanje igralca

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,32 @@ struct zoga
     float polmer;
 };
 
+struct igralec
+{
+    float x;
+    float y;
+    float dolzina;
+    float sirina;
+    float hitrost;
+};
+
 void polje(int sirina, int visina);
 void posodobiZogo(zoga &z, int sirina, int visina);
 void narisiZogo(const zoga &z);
+void posodobiIgralca(igralec &p, int visina);
+void narisiIgralca(igralec &p);
 
 int main()
 {
     const int sirina_zaslona{1800};
     const int visina_zaslona{1100};
 
+    const float dolzina_igralca{240.0f};
+    const float sirina_igralca{30.0f};
+    const float hitrost_igralca{6.0f};
+
     zoga z{sirina_zaslona / 2.0f, visina_zaslona / 2.0f, 5.0f, 4.0f, 20.0f};
+    igralec p{20.0f, visina_zaslona / 2.0f - 15.0f, dolzina_igralca, sirina_igralca, hitrost_igralca};
 
     InitWindow(sirina_zaslona, visina_zaslona, "Pong");
     SetTargetFPS(60);
@@ -26,11 +42,13 @@ int main()
     while (!WindowShouldClose())
     {
         posodobiZogo(z, sirina_zaslona, visina_zaslona);
+        posodobiIgralca(p, visina_zaslona);
 
         BeginDrawing();
         ClearBackground(BLACK);
         polje(sirina_zaslona, visina_zaslona);
         narisiZogo(z);
+        narisiIgralca(p);
         EndDrawing();
     }
 
@@ -57,4 +75,29 @@ void posodobiZogo(zoga &z, int sirina, int visina)
 void narisiZogo(const zoga &z)
 {
     DrawCircle(z.x, z.y, z.polmer, WHITE);
+}
+
+void posodobiIgralca(igralec &p, int visina)
+{
+    if (IsKeyDown(KEY_S))
+    {
+        p.y += p.hitrost;
+    }
+    if (IsKeyDown(KEY_W))
+    {
+        p.y -= p.hitrost;
+    }
+    if (p.y < 0)
+    {
+        p.y = 0;
+    }
+    if (p.y + p.dolzina > visina)
+    {
+        p.y = visina - p.dolzina;
+    }
+}
+
+void narisiIgralca(igralec &p)
+{
+    DrawRectangle(p.x, p.y, p.sirina, p.dolzina, WHITE);
 }
